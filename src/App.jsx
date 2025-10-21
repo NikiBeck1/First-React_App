6bdd39c28220291d91492c329494f33c30cf86c4
NikiBeck1 <niki@wearehamburger.com>
2025-10-21 15:55:59 -0500

    Completed react component state and props exercise

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: ['ready', 'set', 'GO'],
+      text: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.text}
+          onChange={(e) => this.setState({ text: e.target.value })}
+        />
+        <button
+          onClick={() =>
+            this.setState((state) => ({
+              list: state.list.concat(state.text),
+              text: ''
+            }))
+          }
+        >
+          Add
+        </button>
+        <ul>
+          {this.state.list.map((item, index) => {
+            return <li key={index}>{item}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
