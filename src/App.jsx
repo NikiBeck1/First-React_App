678a1c98da165bbfb13d173745fd6963dbfd4f45
Niki Beck <niki@wearehamburger.com>
2025-11-07 15:47:31 -0600

    Updated to use hooks.

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,34 @@ import React from 'react';
 import './App.css';
 import FilmsList from './components/filmsList.jsx';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: ['ready', 'set', 'GO'],
-      text: ''
-    };
-  }
+function App() {
+  const [list, setList] = React.useState(['ready', 'set', 'GO']);
+  const [text, setText] = React.useState('');
 
-  render() {
-    return (
-      <div>
-        <form>
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setList((prevList) => prevList.concat(text));
+    setText('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleAdd}>
         <input
-          value={this.state.text}
-          onChange={(e) => this.setState({ text: e.target.value })}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
-        <button
-          onClick={() =>
-            this.setState((state) => ({
-              list: state.list.concat(state.text),
-              text: ''
-            }))
-          }
-        >
-          Add
-        </button>
-        </form> 
-        <ul>
-          {this.state.list.map((item, index) => {
-            return <li key={index}>{item}</li>;
-          })}
-        </ul>
-        <FilmsList />
-      </div>
-    );
-  }
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {list.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <FilmsList />
+    </div>
+  );
 }
+
 
 export default App;
--- src/components/filmsList.jsx
+++ src/components/filmsList.jsx
@@ -1,32 +1,21 @@
 import React from 'react';
 
-class FilmsList extends React.Component {
-    constructor(props) {
-    super(props);
-     this.state = {
-      films: []
-    }
-}
+function FilmsList(props) {
+    const [films, setFilms] = React.useState([]);
 
-    getFilms() {
-    fetch('https://studioghibliapi-d6fc8.web.app/films')
-        .then(response => response.json())
-        .then(data => {
-            this.setState({ films: data })
-        })
-        .catch(error => console.error('Error fetching films:', error));
-  }
+    React.useEffect(() => {
+        fetch('https://studioghibliapi-d6fc8.web.app/films')
+            .then(response => response.json())
+            .then(data => {
+                setFilms(data);
+            })
+            .catch(error => console.error('Error fetching films:', error));
+    }, []);
 
-  componentDidMount() {
-    this.getFilms();
-  }
-
-    render() {
-    return <ul>
-        {this.state.films.map(film => (
+  return <ul>
+        {films.map(film => (
             <li key={film.id}>{film.title}</li>))}
     </ul>;
-  }
 }
 
 export default FilmsList;
